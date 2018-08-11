b40a9fb29e626a788910c7d8a1a191bb68ae6bea
chenjilan123 <30375990+chenjilan123@users.noreply.github.com>
2018-08-12 02:41:22 +0800

    1

---
+++ SqlServerIndex/SqlServerIndex/SqlFaster/AppFaster.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Data.SqlClient;
+
+namespace SqlServerIndex
+{
+    public class AppFaster
+    {
+        public void CreateScheme()
+        {
+            //SqlConnectionStringBuilder
+        }
+    }
+}
---
+++ SqlServerIndex/SqlServerIndex/SqlFaster/SampleDB.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Text;
+
+namespace SqlServerIndex.SqlFaster
+{
+    public class SampleDB: DbContext
+    {
+
+    }
+}
